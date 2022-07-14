e99494ac339f372858ffe35c4c9c81f341bad26f
guptagaurav288 <guptagaurav288@gmail.com>
2022-07-14 13:31:31 +0530

    pivot element correction

--- PivotElement.java
+++ PivotElement.java
@@ -4,29 +4,20 @@ public class PivotElement {
 		
 	public static void main(String str[]) {
 		
-		int arr[] = {-1,-1,-1,-1,0,-1};
+		int arr[] = {1,3,7,6,5,6};
 		int n = arr.length;
 		System.out.print(findPivot(arr, n));
 	}
 	static int findPivot(int arr[], int n) {
-		int s = 0,e=n-1;
-		int ls = arr[0],rs=arr[n-1];
-		while(s<=e) {
-			if(e==0 || s == n-1) {
-				return -1;
-			}
-			if(ls<rs) {
-				ls=ls+arr[++s];
-			}else if(ls>rs) {
-				rs=rs+arr[--e];
-			}else if(ls==rs && s+1==e-1) {
-				return s+1;
-			}else if(ls == rs){
-				ls=ls+arr[++s];rs=rs+arr[--e];
-			}else {
-				s++;e--;
-			}
-		}
-		return -1;
+		 int sum=0;
+	        for(int i:arr){
+	            sum+=i;
+	        }
+	        int ls=0;
+	        for(int i=0;i<n;i++){
+	            if(ls==sum-ls-arr[i]) return i;
+	            ls+=arr[i];
+	        }
+	        return -1;
 	}
 }
