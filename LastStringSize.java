54acc3623ecdde74f4224a624967e3e704876bb8
guptagaurav288 <guptagaurav288@gmail.com>
2022-07-17 21:53:16 +0530

    String practice

---
+++ LastStringSize.java
@@ -0,0 +1,16 @@
+package gg;
+
+public class LastStringSize {
+    public static void main(String str[]) {
+        System.out.println(findSize("Hello world    "));
+    }
+
+    static int findSize(String str) {
+
+        if (str.equals("")) return -1;
+        String strList[] = str.split(" ");
+
+        String temp = strList[strList.length - 1];
+        return temp.length();
+    }
+}
--- RemoveElement.java
+++ RemoveElement.java
@@ -1,27 +1,21 @@
 package gg;
 
+import java.util.HashMap;
+
 public class RemoveElement {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int arr[] = {1,2,3,4,3,2,5,6,7,2};
+		int nums[] = {1,2,3,4,3,2,5,6,7,2,2,2,2};
 		int k=2;
-		
-		int start =0,end=arr.length-1;
-		
-		while(start<=end) {
-			if(arr[start]==k && arr[end] != k) {
-				arr[start++]=arr[end--];
-			}else {
-				end--;
-				start++;
+		int i =0;
+		for (int j = 0; j < nums.length; j++) {
+			if (nums[j] != k) {
+				nums[i] = nums[j];
+				i++;
 			}
 		}
-		for(int i=0;i<arr.length;i++) {
-			System.out.print(arr[i] + " ");
-		}
-		
-		
+
 	}
 }
