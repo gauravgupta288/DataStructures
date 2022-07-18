aff654eb8dc5eb6c6ddc474777a923071c00f265
guptagaurav288 <guptagaurav288@gmail.com>
2022-07-18 09:43:23 +0530

    String practice

--- LastStringSize.java
+++ LastStringSize.java
@@ -1,12 +1,13 @@
 package gg;
 
+import java.math.BigInteger;
+
 public class LastStringSize {
     public static void main(String str[]) {
         System.out.println(findSize("Hello world    "));
     }
 
     static int findSize(String str) {
-
         if (str.equals("")) return -1;
         String strList[] = str.split(" ");
 
