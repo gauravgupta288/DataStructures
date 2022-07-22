7c654f7fbfab142aadeffcacf63fdeed129e5127
guptagaurav288 <guptagaurav288@gmail.com>
2022-07-22 21:28:47 +0530

    height of water

--- MostWater.java
+++ MostWater.java
@@ -2,18 +2,61 @@ package gg;
 
 public class MostWater {
 
-    public  static void main(String s[]){
-System.out.println(mostWaterContainer(new int[]{1,8,6,2,5,4,8,3,7,10}));
+    public static void main(String s[]) {
+        System.out.println(mostWaterContainerSol3(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
     }
 
-    static int mostWaterContainer(int nums[]){
+    static int mostWaterContainerSol1(int nums[]) {
         int max = 0;
 
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j< nums.length;j++){
-                int num = (Math.min(nums[i], nums[j]) * (j-i));
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int num = (Math.min(nums[i], nums[j]) * (j - i));
                 max = Math.max(max, num);
             }
+        }
+        return max;
+    }
+
+    static int mostWaterContainerSol2(int nums[]) {
+        int max = 0;
+
+        int i = 0, j = nums.length - 1;
+
+        while (i < j) {
+            max = Math.max(max, Math.min(nums[i], nums[j]) * (j - i));
+            if (nums[i] < nums[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return max;
+    }
+
+    /**
+     * Difficulty level hard, width of pillar is 1 mtr long,
+     * minus in between pillar from the array.
+     *
+     * @param nums
+     * @return
+     */
+    static int mostWaterContainerSol3(int nums[]) {
+        int max = 0;
+        int i = 0, j = 1;
+        int sum = 0;
+
+        while (j < nums.length) {
+            if (i == 0 && nums[i] == 0) {
+                i++;
+            } else if (nums[i] <= nums[j]) {
+                max += ((j - i - 1) * (Math.min(nums[i], nums[j]))-sum);
+                i = j;
+                sum=0;
+            }else {
+                sum += nums[j];
+            }
+            j++;
         }
         return max;
     }
