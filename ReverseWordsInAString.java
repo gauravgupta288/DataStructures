fe073b6741fbf4b6f3e1e7b150d242bfe180f7a0
guptagaurav288 <guptagaurav288@gmail.com>
2022-07-22 23:53:36 +0530

    height of water

---
+++ ReverseWordsInAString.java
@@ -0,0 +1,28 @@
+package gg;
+
+public class ReverseWordsInAString {
+    public static void main(String s[]) {
+        System.out.print(reverseSol1("a good   example"));
+    }
+
+    static String reverseSol1(String s) {
+        String str[] = s.split(" ");
+        String s1 = "";
+        for (int i = str.length - 1; i >= 0; i--) {
+            if(!str[i].equals(" ") && !str[i].equals("")){
+            s1 += str[i].trim()+" ";}
+
+        }
+        return s1.trim();
+    }
+    static String reverseSol2(String s) {
+        String str[] = s.split(" ");
+        String s1 = "";
+        for (int i = str.length - 1; i >= 0; i--) {
+            if(!str[i].equals(" ") && !str[i].equals("")){
+                s1 += str[i].trim()+" ";}
+
+        }
+        return s1.trim();
+    }
+}
