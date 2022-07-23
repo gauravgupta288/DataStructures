3101028872edb3d6254acd351d7e90bdb29a02db
guptagaurav288 <guptagaurav288@gmail.com>
2022-07-23 16:56:45 +0530

    find difference

---
+++ SecondLargestInAString.java
@@ -0,0 +1,24 @@
+package gg;
+
+public class SecondLargestInAString {
+    public static void main(String s[]) {
+        System.out.print(secondLargest("dfa1111"));
+    }
+
+    static int secondLargest(String str) {
+        int f = -1, s = -1;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                int num = (str.charAt(i)) - '0';
+                if(num>f){
+                    s=f;
+                    f=num;
+                }else if(num>s && num !=f){
+                    s=num;
+                }
+            }
+        }
+        if(f==s)return -1;
+        return s;
+    }
+}
