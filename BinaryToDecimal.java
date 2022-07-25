d7cbb75a9a114fdfd56e67dca3e21e7a786e9f7d
guptagaurav288 <guptagaurav288@gmail.com>
2022-07-25 22:12:48 +0530

    first unique char in string with O(n) solution

--- BinaryToDecimal.java
+++ BinaryToDecimal.java
@@ -2,13 +2,22 @@ package gg;
 
 public class BinaryToDecimal {
     public static void main(String s[]) {
-
+        binaryToDecimal("1010", "1011");
     }
 
     static void binaryToDecimal(String a, String b) {
 
-        for (int i = 0; i < a.length() && i < b.length(); i++) {
+        int m = a.length();
+        int n = b.length();
+        char[] arr1 = a.toCharArray();
+        char[] arr2 = b.toCharArray();
+        for (int i = 1; i < a.length() && i < b.length(); i++) {
 
+            if (arr1[m - i] == arr2[n - i]) {
+                arr1[m - i] = '0';
+            } else {
+                arr1[m - i] = '1';
+            }
         }
     }
 }
