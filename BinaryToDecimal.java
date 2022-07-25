39d3265dc006cee24b652065bd2bed2c990aab51
guptagaurav288 <guptagaurav288@gmail.com>
2022-07-25 19:54:37 +0530

    first unique char in string with O(n) solution

---
+++ BinaryToDecimal.java
@@ -0,0 +1,14 @@
+package gg;
+
+public class BinaryToDecimal {
+    public static void main(String s[]) {
+
+    }
+
+    static void binaryToDecimal(String a, String b) {
+
+        for (int i = 0; i < a.length() && i < b.length(); i++) {
+
+        }
+    }
+}
--- PlusOne.java
+++ PlusOne.java
@@ -2,14 +2,14 @@ package gg;
 
 public class PlusOne {
     public static void main(String s[]) {
-        System.out.println(plusOne(new int[]{9,8,9,9}));
+        System.out.println(addOneToArray(new int[]{0}));
     }
 
     static int[] plusOne(int arr[]) {
         int n = arr.length - 1;
         int arr1[];
         int carry = 1;
-        if ((arr[0] == 9 &&  arr.length == 1) || (arr[0] == 9 && arr.length != 1  && arr[1] == 9) )  {
+        if ((arr[0] == 9 && arr.length == 1) || (arr[0] == 9 && arr.length != 1 && arr[1] == 9)) {
             arr1 = new int[n + 2];
         } else {
             arr1 = new int[n + 1];
@@ -17,10 +17,10 @@ public class PlusOne {
 
         for (int i = n; i >= 0; i--) {
             if (arr[i] == 9 && carry == 1) {
-                if(i==0){
+                if (i == 0) {
                     arr1[i] = 1;
                     carry = 1;
-                }else{
+                } else {
                     arr1[i] = 0;
                     carry = 1;
                 }
@@ -32,5 +32,19 @@ public class PlusOne {
             }
         }
         return arr1;
+    }
+
+    static int[] addOneToArray(int[] arr) {
+        int n = arr.length;
+        for (int i = n-1; i >= 0; i--){
+            if(arr[i]<9){
+                arr[i] = arr[i] + 1;
+                return arr;
+            }
+            arr[i] = 0;
+        }
+        int []arr1 =new int[n+1];
+        arr1[0] = 1;
+            return arr1;
     }
 }
