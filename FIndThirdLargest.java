cabb17fcb6e3cb169f1f36a451030f26473dead6
guptagaurav288 <guptagaurav288@gmail.com>
2022-07-27 21:17:52 +0530

    Third largest clean code, O(n) time complexity

--- FIndThirdLargest.java
+++ FIndThirdLargest.java
@@ -3,13 +3,9 @@ package gg;
 public class FIndThirdLargest {
 
     public static void main(String s[]) {
-        int a[] = {3,2,1};
+        int a[] = {3, 2,3,4,1};
         int third = thirdLargestSol2(a);
-        if (third == -1) {
-            System.out.println("No third largest number in this array ");
-        } else {
-            System.out.println("Third largest number in array " + third);
-        }
+        System.out.println("third largest number in this array " + third);
     }
 
     static int thirdLargest(int arr[]) {
@@ -42,21 +38,26 @@ public class FIndThirdLargest {
     }
 
     static int thirdLargestSol2(int[] arr) {
-        int first = arr[0], second = arr[0], third = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > first) {
-                third=second;
-                second=first;
-                first=arr[i];
-            }  if ((arr[i] > second && arr[i] < first) || (first==second)) {
-                third = second;
-                second = arr[i];
-            }  if ((arr[i] > third && arr[i] < second)||(third==second)) {
-                third = arr[i];
-            }
+        Integer max1 = null;
+        Integer max2 = null;
+        Integer max3 = null;
 
+
+        for (Integer num : arr) {
+            if (num.equals(max1) || num.equals(max2) || num.equals(max3)) continue;
+
+            if (max1 == null || num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (max2 == null || num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (max3 == null || num > max3) {
+                max3 = num;
+            }
         }
-        if(second==third)return first;
-        return third;
+
+        return max3 == null ? max1 : max3;
     }
 }
