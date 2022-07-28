656b997cf1c746ea4d768d3dc0c3f7556b417194
guptagaurav288 <guptagaurav288@gmail.com>
2022-07-28 10:24:35 +0530

    reverse only character 2 solution, O(n)

--- ReverseOnlyString.java
+++ ReverseOnlyString.java
@@ -2,27 +2,51 @@ package gg;
 
 public class ReverseOnlyString {
     public static void main(String[] args) {
-        reverse("Test1ng-Leet=code-Q!");
+        System.out.println(reverseStringSol2("a-bC-dEf-ghIj"));
     }
-    static void reverse(String s){
+
+    static String reverse(String s) {
         int len = s.length();
 
         Character ch[] = new Character[len];
         String s1 = "";
-        for(int i=0;i<len;i++){
-            if(!Character.isLetterOrDigit(s.charAt(i))){
+        for (int i = 0; i < len; i++) {
+            if (!Character.isLetter(s.charAt(i))) {
                 ch[i] = s.charAt(i);
-            }else{
-                s1+=s.charAt(i);
+            } else {
+                s1 += s.charAt(i);
             }
         }
 
-        int k=0;
-        int m=s1.length()-1;
-        for(int i=0;i<ch.length;i++){
-            if(ch[i]==null){
-               ch[i] = s1.charAt(m--);
+        int m = s1.length() - 1;
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] == null) {
+                ch[i] = s1.charAt(m--);
             }
         }
+        String str = "";
+        for (int i = 0; i < ch.length; i++) {
+            str += ch[i];
+        }
+        return str;
+    }
+
+    static String reverseStringSol2(String str) {
+        int s = 0;
+        int e = str.length() - 1;
+        StringBuilder sb = new StringBuilder(str);
+        while (s <= e) {
+            char ch1 = str.charAt(s);
+            char ch2 = str.charAt(e);
+            if (Character.isLetter(ch1) && Character.isLetter(ch2)) {
+                sb.setCharAt(s++, ch2);
+                sb.setCharAt(e--, ch1);
+            } else if (!Character.isLetter(ch1)) {
+                s++;
+            } else if (!Character.isLetter(ch2)) {
+                e--;
+            }
+        }
+        return sb.toString();
     }
 }
