eb6631d1a2ad2183c79e4227ccccb9fa111fa6a4
guptagaurav288 <guptagaurav288@gmail.com>
2022-08-01 19:59:37 +0530

    merge 2 sorted array

--- Arrays/src/test/java/MergeTwoSortedArrays.java
+++ Arrays/src/test/java/MergeTwoSortedArrays.java
@@ -1,9 +1,9 @@
 public class MergeTwoSortedArrays {
     public static void main(String[] args) {
-        int arr1[] = {1, 2,4, 5,};
-        int arr2[] = {4, 7,8,8};
+        int arr1[] = {1, 2, 3, 0, 0, 0};
+        int arr2[] = {2, 5, 6};
 
-        mergeArrays(arr1, arr2);
+        mergeSortedArray(arr1, arr1.length, arr2, arr2.length);
     }
 
     static void mergeArrays(int[] arr1, int[] arr2) {
@@ -15,21 +15,43 @@ public class MergeTwoSortedArrays {
         int k = 0;
 
         int arr3[] = new int[m + n];
-        while (j < n && i<m) {
+        while (j < n && i < m) {
             if (arr1[i] < arr2[j]) {
                 arr3[k++] = arr1[i++];
             } else if (arr1[i] > arr2[j]) {
                 arr3[k++] = arr2[j++];
-            }else{
+            } else {
                 arr3[k++] = arr1[i];
                 i++;
             }
         }
-        while(i<m){
+        while (i < m) {
             arr3[k++] = arr1[i++];
         }
-        while(j<n){
+        while (j < n) {
             arr3[k++] = arr2[j++];
+        }
+    }
+
+    static void mergeSortedArray(int[] nums1, int m, int[] nums2, int n) {
+        int i = 0;
+        int j = 0;
+
+        while (i < m && j < n) {
+            if (nums1[i] == 0) {
+                nums1[i++] = nums2[j++];
+            } else if (nums1[i] <= nums2[j]) {
+                i++;
+            } else if (nums1[i] >= nums2[j]) {
+                nums1[i + 1] = nums1[i];
+                nums1[i++] = nums2[j++];
+
+            }
+
+
+        }
+        while (j < n) {
+            nums1[i++] = nums2[j++];
         }
     }
 }
