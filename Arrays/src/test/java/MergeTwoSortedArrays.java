28ba1daae4729829054dc16eee0a0e5a966f92d0
guptagaurav288 <guptagaurav288@gmail.com>
2022-08-01 20:16:50 +0530

    merge 2 sorted array with simplest logic

--- Arrays/src/test/java/MergeTwoSortedArrays.java
+++ Arrays/src/test/java/MergeTwoSortedArrays.java
@@ -34,24 +34,19 @@ public class MergeTwoSortedArrays {
     }
 
     static void mergeSortedArray(int[] nums1, int m, int[] nums2, int n) {
-        int i = 0;
-        int j = 0;
+        int i = m - 1;
+        int j = n - 1;
+        int k = m+n-1;
 
-        while (i < m && j < n) {
-            if (nums1[i] == 0) {
-                nums1[i++] = nums2[j++];
-            } else if (nums1[i] <= nums2[j]) {
-                i++;
-            } else if (nums1[i] >= nums2[j]) {
-                nums1[i + 1] = nums1[i];
-                nums1[i++] = nums2[j++];
-
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
             }
-
-
         }
-        while (j < n) {
-            nums1[i++] = nums2[j++];
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
         }
     }
 }
