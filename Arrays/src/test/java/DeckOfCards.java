65d80abc8aeb3208714657cbcb9d91bb7a95534f
guptagaurav288 <guptagaurav288@gmail.com>
2022-08-04 17:40:44 +0530

    remove one element to check increment order array

--- Arrays/src/test/java/DeckOfCards.java
+++ Arrays/src/test/java/DeckOfCards.java
@@ -1,25 +1,22 @@
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class DeckOfCards {
     public static void main(String[] args) {
-        System.out.println(deckOfCards(new int[]{1,1,1,2,2,2,3,3}));
+        System.out.println(hasGroupsSizeX(new int[]{1, 1, 1,1, 2, 2, 4, 4, 4, 4,4,4}));
     }
 
-    static boolean deckOfCards(int arr[]) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (map.containsKey(arr[i])) {
-                map.put(arr[i], map.get(arr[i]) + 1);
-            } else {
-                map.put(arr[i], 1);
-            }
-        }
-        int temp = map.get(arr[0]);
-        for (int i = 1; i < arr.length; i++) {
-            if (temp != map.get(arr[i])) {
-                return false;
-            }
-        }
-        return true;
+    public static boolean hasGroupsSizeX(int[] deck) {
+        Map<Integer, Integer> count = new HashMap<>();
+        int res = 0;
+        for (int i : deck) count.put(i, count.getOrDefault(i, 0) + 1);
+        for (int i : count.values()) res = gcd(i, res);
+        return res > 1;
+    }
+
+    public static int gcd(int a, int b) {
+        return b > 0 ? gcd(b, a % b) : a;
     }
 }
