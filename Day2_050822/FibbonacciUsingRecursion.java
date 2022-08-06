268d62ca8782f25e626612a63485c9fe523ecbc3
guptagaurav288 <guptagaurav288@gmail.com>
2022-08-06 15:34:58 +0530

    set mismatch

---
+++ Day2_050822/FibbonacciUsingRecursion.java
@@ -0,0 +1,15 @@
+package gg.Day2_050822;
+
+public class FibbonacciUsingRecursion {
+    public static void main(String[] args) {
+
+    }
+
+    static int fibbo(int n1, int n2){
+        int i = 1;
+        int j = 1;
+        int res = i+j;
+        System.out.println(res);
+        return fibbo(res,j);
+    }
+}
---
+++ Day3_060822/SetMismatch.java
@@ -0,0 +1,23 @@
+package gg.Day3_060822;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetMismatch {
+    public static void main(String[] args) {
+        System.out.println(setMismatch(new int[]{2,3,2}));
+    }
+
+    static int[] setMismatch(int nums[]) {
+        int arr[] = new int[2];
+        int sum = 0;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0;  i < nums.length; i++) {
+            if(set.contains(nums[i])) arr[0] = nums[i];
+            else set.add(nums[i]);
+            sum += nums[i];
+        }
+        arr[1] = ((nums.length)* (nums.length+1))/2 - (sum - arr[0]);
+        return arr;
+    }
+}
