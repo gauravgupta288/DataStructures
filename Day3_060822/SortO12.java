0a68af8a5fb53aa9f7a4811aadd1624be7de97f5
guptagaurav288 <guptagaurav288@gmail.com>
2022-08-07 13:35:08 +0530

    find n and its double in a array

---
+++ Day3_060822/SortO12.java
@@ -0,0 +1,46 @@
+package gg.Day3_060822;
+
+public class SortO12 {
+    public static void main(String[] args) {
+System.out.println(sort(new int[]{0,0,1,0,1,1}));
+    }
+
+    static int sort(int[] nums) {
+
+        int s = 0;
+        int i = 0;
+        int j = nums.length-1;
+
+        while (i<j){
+
+            if( nums[s]==0)s++;
+            if(nums[j]==2)j--;
+
+            if(nums[i]==2){
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j--;
+
+            } else if(nums[i]==0){
+                int temp = nums[s];
+                nums[s] = nums[i];
+                nums[i] = temp;
+                s++;
+
+            }
+            if(i>=j){
+                break;
+            }
+            if (nums[j] == 0) {
+                int temp = nums[s];
+                nums[s] = nums[j];
+                nums[j] = temp;
+                s++;
+            }
+            i++;
+        }
+
+        return 0;
+    }
+}
