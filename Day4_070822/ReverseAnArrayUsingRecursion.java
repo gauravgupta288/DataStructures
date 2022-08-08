ee43de4ce7d3cc4c86eac8b436aeef156a398784
guptagaurav288 <guptagaurav288@gmail.com>
2022-08-08 08:43:56 +0530

    recursion

--- Day4_070822/ReverseAnArrayUsingRecursion.java
+++ Day4_070822/ReverseAnArrayUsingRecursion.java
@@ -1,24 +1,23 @@
 package gg.Day4_070822;
 
 public class ReverseAnArrayUsingRecursion {
-    static int[] arr;
 
     public static void main(String[] args) {
-        arr = new int[]{1, 2, 3, 4, 5};
-        reverseArray(0, arr.length - 1);
+        char arr[] = new char[]{1, 2, 3, 4, 5};
+        reverse(0, arr.length - 1, arr);
     }
 
-    static void reverseArray(int i, int j) {
-
+    static void reverse(int i, int j, char arr[]){
         if (i >= j) return;
 
-        swap(i, j);
-        reverseArray(i + 1, j-1);
+        swap(i, j, arr);
+        reverse(i + 1, j-1, arr);
+
     }
 
-    static void swap(int l, int r) {
-        int temp = arr[l];
-        arr[l] = arr[r];
-        arr[r] = temp;
+    static void swap(int l, int r, char s[]) {
+        char temp = s[l];
+        s[l] = s[r];
+        s[r] = temp;
     }
 }
