6e0d149ff7d85147108d66ba22d68e0f80746f35
guptagaurav288 <guptagaurav288@gmail.com>
2022-08-13 18:39:23 +0530

    equi points

--- Day010_130822/TripletInAArray.java
+++ Day010_130822/TripletInAArray.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 public class TripletInAArray {
     public static void main(String[] args) {
-        System.out.println(triplet(new int[]{1, 2, 2, 1,5}, 5, 8));
+        System.out.println(triplet(new int[]{1, 2, 2, 1,}, 4, 3));
     }
 
     static boolean triplet(int A[], int n, int X) {
@@ -14,7 +14,7 @@ public class TripletInAArray {
             Set<Integer> list = new HashSet<>();
             for (int j = i + 1; j < n; j++) {
                 int num = X - A[j] - A[i];
-                if (list.contains(num) && A[i] != num && A[j] != num){
+                if (list.contains(num)) {
                     return true;
                 }
                 list.add(A[j]);
