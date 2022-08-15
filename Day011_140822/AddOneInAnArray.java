47ee1b9ac039c20635bf5f9fa9ced4dd124c1adf
guptagaurav288 <guptagaurav288@gmail.com>
2022-08-15 12:49:33 +0530

    leader

---
+++ Day011_140822/AddOneInAnArray.java
@@ -0,0 +1,30 @@
+package gg.Day011_140822;
+
+import java.util.Vector;
+
+public class AddOneInAnArray {
+    public static void main(String[] args) {
+addOne(new int[]{1,2,3,4}, 4);
+    }
+
+    static int[] addOne(int arr[],int n) {
+        int carry = 1;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int num = arr[i] + carry;
+            if (num == 10) {
+                carry = 1;
+                arr[i] = 0;
+            } else {
+                carry = 0;
+                arr[i] = num;
+            }
+        }
+        if (carry == 1) {
+            int newArray[] = new int[arr.length+1];
+            newArray[0] =1;
+            return newArray;
+        }
+
+        return arr;
+    }
+}
---
+++ Day011_140822/LongestPrefixInStringArray.java
@@ -0,0 +1,23 @@
+package gg.Day011_140822;
+
+import java.util.Arrays;
+
+public class LongestPrefixInStringArray {
+    public static void main(String[] args) {
+        System.out.print(longestPrefix(new String[]{"geeksforgeeks", "geeks", "geek", "hgeezer"}, 4));
+    }
+
+    static String longestPrefix(String arr[], int n) {
+        StringBuilder sb = new StringBuilder();
+        Arrays.sort(arr);
+        for (int i = 0; i < arr[0].length() && i< arr[n-1].length(); i++) {
+            if(arr[0].charAt(i) != arr[n-1].charAt(i)){
+                break;
+            }else{
+                sb.append(arr[0].charAt(i));
+            }
+
+        }
+        return "".equals(sb.toString()) ? "-1" : sb.toString();
+    }
+}
