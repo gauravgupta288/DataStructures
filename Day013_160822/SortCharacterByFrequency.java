aaa3dd1e82b9e4cb49566a9ba7445312a2f80141
guptagaurav288 <guptagaurav288@gmail.com>
2022-08-16 22:28:49 +0530

    sort characters based on frequency

---
+++ Day013_160822/SortCharacterByFrequency.java
@@ -0,0 +1,38 @@
+package gg.Day013_160822;
+
+import java.util.HashMap;
+
+public class SortCharacterByFrequency {
+    public static void main(String[] args) {
+        System.out.print(sortChar("cccaaa"));
+    }
+
+    static String sortChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        char ch1 = '0';
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        while (!map.isEmpty()) {
+            int max = 0;
+            for (char ch : map.keySet()) {
+                if (max < map.get(ch)) {
+                    max = map.get(ch);
+                    ch1 = ch;
+                }
+            }
+            add(sb, ch1, max);
+            map.remove(ch1);
+        }
+        return sb.toString();
+    }
+
+    static StringBuilder add(StringBuilder sb, char ch, int frequency) {
+        for (int i = 0; i < frequency; i++) {
+            sb.append(ch);
+        }
+        return sb;
+    }
+}
