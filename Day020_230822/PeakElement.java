7561ca3861a33c5fde01c15212ed35b68ee44504
guptagaurav288 <guptagaurav288@gmail.com>
2022-08-24 17:23:18 +0530

    smaller than itself O(n) solution

--- Day020_230822/PeakElement.java
+++ Day020_230822/PeakElement.java
@@ -2,24 +2,25 @@ package gg.Day020_230822;
 
 public class PeakElement {
     public static void main(String[] args) {
-        System.out.println(peakElement(new int[]{4,1,2,3}, 4));
+        System.out.println(peakElement(new int[]{4, 1, 2, 3}, 4));
     }
 
     static int peakElement(int arr[], int n) {
         if (n == 1) return 0;
-        for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                if (arr[i] > arr[i + 1]) {
-                    return i;
-                }
-            } else if (i == n - 1) {
-                if (arr[i] > arr[i - 1]){
-                    return i;
-                }
+        int start = 0;
+        int end = n - 1;
+        if(arr[0]>arr[1])return 0;
+        if(arr[n-1]>arr[n-2]) return n-1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
             } else {
-                if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
-                    return i;
-                }
+                start = mid;
             }
         }
         return -1;
---
+++ Day020_230822/SticklerThief.java
@@ -0,0 +1,22 @@
+package gg.Day020_230822;
+
+public class SticklerThief {
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{5,5,10,100,10,5}, 6));
+    }
+
+    static int maxProfit(int arr[], int n) {
+
+        if(n==1) return arr[0];
+        int profit1 = 0;
+        int profit2 = 0;
+
+        for (int i = 0; i < n; i = i + 2) {
+            profit1 += arr[i];
+        }
+        for (int i = 1; i < n; i = i + 2) {
+            profit2 += arr[i];
+        }
+        return  Math.max(profit1, profit2);
+    }
+}
