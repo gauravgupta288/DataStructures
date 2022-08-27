2e07e26e5b4804f125f05e907a5b6a6cd4d70fc4
guptagaurav288 <guptagaurav288@gmail.com>
2022-08-27 23:47:55 +0530

    Easy and Medium question solved of leetCode

---
+++ Day024_270822/GenericType.java
@@ -0,0 +1,21 @@
+package gg.Day024_270822;
+
+public class GenericType<T> {
+    private T t;
+
+    public void add(T t, T t1) {
+        System.out.println("Type of T is " + t + t1);
+        System.out.println(t instanceof String);
+    }
+
+    public static void main(String[] args) {
+        String s = "Bunny";
+        int age = 1;
+
+        int a = Math.abs(-1);
+
+        GenericType gt = new GenericType();
+        gt.add(s, " Ryan");
+        gt.add(age, 10);
+    }
+}
---
+++ Day024_270822/WateringPlants.java
@@ -0,0 +1,26 @@
+package gg.Day024_270822;
+
+public class WateringPlants {
+    public static void main(String[] args) {
+        System.out.println(wateringPlant(new int[]{3,2,4,2,1}, 6));
+    }
+
+    static int wateringPlant(int[] plants, int capacity) {
+        int count = 0;
+        int temp = capacity;
+
+
+        for (int i = 0; i < plants.length; i++) {
+
+            if (capacity >= plants[i]) {
+                capacity -= plants[i];
+                count++;
+            } else if (capacity < plants[i]) {
+                count += i * 2 + 1;
+                capacity =  temp;
+                capacity -= plants[i];
+            }
+        }
+        return count;
+    }
+}
