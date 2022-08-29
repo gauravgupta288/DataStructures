bf344a01d7a229f6f49318e966b6bd99fe9d8357
guptagaurav288 <guptagaurav288@gmail.com>
2022-08-29 23:17:36 +0530

    replace string

---
+++ Day026_290822/ReplaceCharWithDigit.java
@@ -0,0 +1,19 @@
+package gg.Day026_290822;
+
+public class ReplaceCharWithDigit {
+
+    public static void main(String[] args) {
+        System.out.println(replaceAll("a1b1c1"));
+    }
+
+    static String replaceAll(String s) {
+        char ch[] = s.toCharArray();
+
+        for (int i = 0; i < ch.length; i++) {
+            if (Character.isDigit(ch[i])) {
+                ch[i] = ((char)((ch[i-1]) + (ch[i] - '0')));
+            }
+        }
+        return String.valueOf(ch);
+    }
+}
