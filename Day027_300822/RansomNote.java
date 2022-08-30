365476d9263b7cfba44ff71fc1d3389d141c60ba
guptagaurav288 <guptagaurav288@gmail.com>
2022-08-30 23:42:14 +0530

    ransom string

--- Day027_300822/RansomNote.java
+++ Day027_300822/RansomNote.java
@@ -28,15 +28,12 @@ public class RansomNote {
 
     static boolean ransomNoteSol2(String ransomNote, String magazine) {
         int letters[] = new int[26];
-
         for (int i = 0; i < ransomNote.length(); i++) {
             letters[ransomNote.charAt(i) - 'a']++;
         }
-
         for (int i = 0;  i < magazine.length(); i++) {
             letters[magazine.charAt(i) - 'a']--;
         }
-
         for(int i:letters){
             if(i > 0){
                 return false;
