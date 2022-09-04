f0304bdfba7bbbbcade625f8973bea44e11069df
guptagaurav288 <guptagaurav288@gmail.com>
2022-09-04 23:34:41 +0530

    sort array

--- Month02_Sep/Day032_040922/RelativeSortArray.java
+++ Month02_Sep/Day032_040922/RelativeSortArray.java
@@ -5,7 +5,7 @@ import java.util.TreeMap;
 
 public class RelativeSortArray {
     public static void main(String[] args) {
-        sortArray(new int[]{2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19}, new int[]{2, 1, 4, 3, 9, 6});
+        sortArraySol2(new int[]{2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19}, new int[]{2, 1, 4, 3, 9, 6});
     }
 
     static int[] sortArray(int[] arr1, int[] arr2) {
@@ -34,5 +34,38 @@ public class RelativeSortArray {
             }
         }
         return res;
+    }
+
+    static int[] sortArraySol2(int[] arr1, int[] arr2) {
+        int res[] = new int[1000];
+        int finalArray[] = new int[arr1.length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            res[arr1[i]]++;
+        }
+        int j = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            int k = 0;
+            int count = res[arr2[i]];
+            while (k < count) {
+                finalArray[j++] = arr2[i];
+                k++;
+            }
+            res[arr2[i]] = 0;
+        }
+
+        for (int i = 1; i < 1001; i++) {
+            int count = res[i];
+            int k = 0;
+            while (k < count) {
+                finalArray[j++] = i;
+                k++;
+
+            }
+            if(j== arr1.length){
+                break;
+            }
+        }
+        return finalArray;
     }
 }
