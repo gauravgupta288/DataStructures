b0cd339fb06ff1773e71d49e9bf01f6e938c6a56
guptagaurav288 <guptagaurav288@gmail.com>
2022-09-10 21:20:47 +0530

    count number

--- Month02_Sep/Day038_100922/CountTheNumberOfConsistentStrings.java
+++ Month02_Sep/Day038_100922/CountTheNumberOfConsistentStrings.java
@@ -2,7 +2,7 @@ package gg.Month02_Sep.Day038_100922;
 
 public class CountTheNumberOfConsistentStrings {
     public static void main(String[] args) {
-        System.out.println(countNumbers("abc", new String[]{"a","b","c","ab","ac","bc","abc"}));
+        System.out.println(countDistinctNumbersSol2("abc", new String[]{"a", "b", "c", "ab", "ac", "bc", "abc"}));
     }
 
     static int countNumbers(String allowed, String[] words) {
@@ -23,6 +23,26 @@ public class CountTheNumberOfConsistentStrings {
             }
         }
 
+        return count;
+    }
+
+    static int countDistinctNumbersSol2(String allowed, String[] words) {
+        int count = words.length;
+        int arr[] = new int[26];
+        for (int i = 0; i < allowed.length(); i++) {
+            arr[allowed.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < words.length; i++) {
+            int j = 0;
+            while ( j < words[i].length()){
+                if(arr[words[i].charAt(j)-'a'] == 0){
+                    count--;
+                    break;
+                }
+                j++;
+            }
+        }
         return count;
     }
 }
