016b28f443df6c516bdac0ac37b24713e2e32852
guptagaurav288 <guptagaurav288@gmail.com>
2022-09-14 21:27:52 +0530

    binary search book allocation problem

--- Month02_Sep/Day041_130922/BookAllocationProblem.java
+++ Month02_Sep/Day041_130922/BookAllocationProblem.java
@@ -2,41 +2,41 @@ package gg.Month02_Sep.Day041_130922;
 
 public class BookAllocationProblem {
     public static void main(String[] args) {
-        System.out.println(bookAllocation(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5));
+        System.out.println(bookAllocation(new int[]{1,2,3,1,1}, 4));
     }
 
     static int bookAllocation(int[] weights, int days) {
-        int s = 0;
-        int e = weights.length - 1;
-        int minCount = 0;
+        int s = 1;
         int sum = 0;
+        int ans = -1;
 
         for (int i : weights) {
             sum += i;
         }
-        while (s <= sum) {
-            int mid = s + (e - s) / 2;
+        while (s < sum) {
+            int mid = s + (sum - s) / 2;
             if (isPossibleSol(weights, days, mid)) {
-                e = mid - 1;
+                ans = mid;
+                sum = mid - 1;
             } else {
                 s = mid + 1;
             }
         }
-        return minCount;
+        return ans;
     }
 
     static boolean isPossibleSol(int[] weights, int days, int mid) {
         int ans = 0;
-        int dayCount = 0;
+        int dayCount = 1;
         for (int i : weights) {
-            ans += i;
-
-            if (ans >= mid) {
-                dayCount++;
+            if (ans + i <= mid) {
+                ans += i;
             } else {
-                if(dayCount>days){
+                dayCount++;
+                if (dayCount > days || i > mid) {
                     return false;
                 }
+                ans = i;
             }
         }
         return true;
