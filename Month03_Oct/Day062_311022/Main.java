712604f084ee5996ebba1e3caa8c73476784b793
guptagaurav288 <guptagaurav288@gmail.com>
2022-10-31 22:46:41 +0530

    moveZero

--- Month03_Oct/Day062_311022/Main.java
+++ Month03_Oct/Day062_311022/Main.java
@@ -4,4 +4,27 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
+
+    public static class MoveZerosToLast {
+        public static void main(String[] args) {
+            moveZero(new int[]{0, 1, 0, 3, 12});
+
+        }
+
+        static void moveZero(int[] arr) {
+
+            int k = 0;
+            int zc = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] != 0) {
+                    arr[k++] = arr[i];
+                } else {
+                    zc++;
+                }
+            }
+            while(k< arr.length){
+                arr[k++] = 0;
+            }
+        }
+    }
 }
