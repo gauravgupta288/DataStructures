35df4f1a03a649912f38ebdc2e77e963a54d3010
guptagaurav288 <guptagaurav288@gmail.com>
2022-10-31 07:49:57 +0530

    bubbleSort

--- Month03_Oct/Day061_301022/ReverseTheArrayFromGivenIndex.java
+++ Month03_Oct/Day061_301022/ReverseTheArrayFromGivenIndex.java
@@ -4,10 +4,21 @@ import java.util.ArrayList;
 
 public class ReverseTheArrayFromGivenIndex {
     public static void main(String[] args) {
-
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(0);list.add(1);list.add(2);list.add(3);list.add(4);list.add(5);list.add(6);
+        reverseTheArrayFromGivenIndex(list, 3);
     }
 
-    static void ReverseTheArrayFromGivenIndex(ArrayList<Integer> arr, int m){
+    static void reverseTheArrayFromGivenIndex(ArrayList<Integer> arr, int m){
 
+        int end = arr.size() - 1;
+        int start = m + 1;
+
+        while (start < end){
+            int temp = arr.get(start);
+            arr.set(start, arr.get(end));
+            arr.set(end, temp);
+            start++;end--;
+        }
     }
 }
