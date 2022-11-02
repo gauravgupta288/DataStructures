0a622557a36298e2170e133478a6b3ae2ff326cb
guptagaurav288 <guptagaurav288@gmail.com>
2022-11-02 21:18:45 +0530

    SumOfTwoArrays

--- Month04_Nov/Day063_011122/SumOfTwoArrays.java
+++ Month04_Nov/Day063_011122/SumOfTwoArrays.java
@@ -1,33 +1,30 @@
 package Month04_Nov.Day063_011122;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SumOfTwoArrays {
     public static void main(String[] args) {
-        sum(new int[]{9,9,9}, 3, new int[]{9,9,9},3);
+        sum(new int[]{9,9,9,9,9,9,9,9,9,9}, 1);
     }
 
-    static int[] sum(int[] a, int n, int[] b, int m) {
-        // Write your code here.
-        int sum1 = 0;
-        int sum2 = 0;
-        int z = Math.max(n,m);
-
-
-        for (int i : a) {
+    static List<Integer> sum(int[] num, int k) {
+        long sum1 = 0;
+        List<Integer> list = new ArrayList<>();
+        for (int i : num) {
             sum1 = sum1 * 10 + i;
         }
-        for (int i : b) {
-            sum2 = sum2 * 10 + i;
-        }
-        int x = sum1 + sum2;
-        String num = Integer.toString(x);
-        int len = num.length();
-        int[] res = new int[len];
-        while (x != 0) {
-            res[len-1] = x %  10;
-            x /= 10;
-            len--;
-        }
+        long x = sum1 + (long)k;
 
-        return res;
+        long sum = 0;
+        while (x != 0) {
+            sum = sum * 10 + x % 10;
+            x /= 10;
+        }
+        while (sum != 0) {
+            list.add( (int)sum % 10);
+            sum /= 10;
+        }
+        return list;
     }
 }
