b3e85f7fc8170e7990fd0045badd6bdf6b3cb561
guptagaurav288 <guptagaurav288@gmail.com>
2022-11-09 13:50:22 +0530

    LinkedList

---
+++ Month04_Nov/Day070_091122/HollowRactangle.java
@@ -0,0 +1,21 @@
+package Month04_Nov.Day070_091122;
+
+public class HollowRactangle {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+
+                if(i ==0 || i == n-1 || j ==0 || j == n -1){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
