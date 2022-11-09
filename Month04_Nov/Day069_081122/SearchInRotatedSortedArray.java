48e2b95969a0302a906e2238ae1624e621da409c
guptagaurav288 <guptagaurav288@gmail.com>
2022-11-09 12:13:33 +0530

    SearchInRotatedSortedArray

--- Month04_Nov/Day069_081122/SearchInRotatedSortedArray.java
+++ Month04_Nov/Day069_081122/SearchInRotatedSortedArray.java
@@ -2,21 +2,32 @@ package Month04_Nov.Day069_081122;
 
 public class SearchInRotatedSortedArray {
     public static void main(String[] args) {
-        System.out.println(search(new int[]{4, 5, 6, 7, 8, 0, 1, 2, 3}, 0));
+        System.out.println(search(new int[]{4, 5, 6, 7, 8, 0, 1, 2, 3}, 4));
     }
 
     static int search(int arr[], int target) {
 
         int start = 0;
         int end = arr.length - 1;
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            if (arr[mid] == target) {
+        while (start <= end) {
+
+            int mid = start + (end - start)/2;
+
+            if(arr[mid] == target){
                 return mid;
-            } else if (target > arr[start] && target < arr[mid]) {
-                end = mid;
-            } else if (target < arr[mid] && target < arr[end]) {
-                start = mid + 1;
+            }
+            if(arr[start] <= arr[mid]){
+                if(target < arr[start] || target > arr[mid]){
+                    start = mid + 1;
+                }else{
+                    end = mid - 1;
+                }
+            }else{
+                if(target > arr[end] || target < arr[mid]){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
             }
         }
         return -1;
