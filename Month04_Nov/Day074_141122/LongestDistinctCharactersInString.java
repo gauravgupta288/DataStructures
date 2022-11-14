ad9a8ba498fe009d52b5468c87c8f2e30518462a
guptagaurav288 <guptagaurav288@gmail.com>
2022-11-14 22:11:34 +0530

    LongestDistinctCharactersInString

---
+++ Month04_Nov/Day074_141122/LongestDistinctCharactersInString.java
@@ -0,0 +1,19 @@
+package Month04_Nov.Day074_141122;
+
+public class LongestDistinctCharactersInString {
+    public static void main(String[] args) {
+
+    }
+
+    static void longestDistinct(String S) {
+
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < S.length(); i++) {
+            S.
+        }
+
+
+    }
+}
