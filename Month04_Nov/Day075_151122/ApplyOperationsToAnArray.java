4753248d12ec6f7bfa732b675cbf7f865cd87251
guptagaurav288 <guptagaurav288@gmail.com>
2022-11-15 22:47:45 +0530

    ApplyOperationsToAnArray

--- Month04_Nov/Day075_151122/ApplyOperationsToAnArray.java
+++ Month04_Nov/Day075_151122/ApplyOperationsToAnArray.java
@@ -8,7 +8,6 @@ public class ApplyOperationsToAnArray {
     }
 
     static int[] applyOperation(int[] arr){
-
         int k = 0;
         for(int i = 1; i < arr.length; i++){
             if(arr[i] == arr[i - 1]){
@@ -16,14 +15,12 @@ public class ApplyOperationsToAnArray {
                 arr[i] = 0;
             }
         }
-
         for(int i = 0;i< arr.length; i++){
             if(arr[i] > 0){
                 arr[k++] = arr[i];
                 //arr[i] = 0;
             }
         }
-
         for(int i = k;i< arr.length;i++){
             arr[i] = 0;
         }
