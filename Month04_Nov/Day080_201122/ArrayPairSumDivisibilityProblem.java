187822732549b80bdb5d8b711de5414e3e735294
guptagaurav288 <guptagaurav288@gmail.com>
2022-11-20 20:18:23 +0530

    ArrayPairSumDivisibilityProblem

--- Month04_Nov/Day080_201122/ArrayPairSumDivisibilityProblem.java
+++ Month04_Nov/Day080_201122/ArrayPairSumDivisibilityProblem.java
@@ -12,7 +12,6 @@ public class ArrayPairSumDivisibilityProblem {
         }
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                if((nums[j] != -1 && nums[i] != -1) && (nums[i] + nums[j]) % k ==0){
                     nums[i] = -1;
                     nums[j] = -1;
                     break;
