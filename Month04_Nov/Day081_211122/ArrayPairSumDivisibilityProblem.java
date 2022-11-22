5deeb7ae74ccddb612cbaa1e4ceee064eb72f487
guptagaurav288 <guptagaurav288@gmail.com>
2022-11-22 18:27:43 +0530

    ArrayPairSumDivisibilityProblem

--- Month04_Nov/Day081_211122/ArrayPairSumDivisibilityProblem.java
+++ Month04_Nov/Day081_211122/ArrayPairSumDivisibilityProblem.java
@@ -1,35 +1,50 @@
 package Month04_Nov.Day081_211122;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class ArrayPairSumDivisibilityProblem {
+
     public static void main(String[] args) {
-        System.out.println(arrayPair(new int[]{14,2,10,2}, 2));
+        System.out.println(arrayPair(new int[]{14, 2, 10, 2}, 2));
     }
 
     static boolean arrayPair(int[] nums, int k) {
         if (nums.length % 2 != 0) return false;
         HashMap<Integer, Integer> map = new HashMap<>();
 
+        int x = 0;
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i] % k, map.getOrDefault(nums[i] % k, 0) + 1);
+            x = nums[i] % k;
+            if (x < 0) {
+                x += k;
+            }
+            map.put(x, map.getOrDefault(x, 0) + 1);
         }
 
-        for (int i : map.keySet()) {
-            int x = k - i;
-            if(i==0 && map.get(i) %2 != 0) {
-                return false;
-            }else{
-                if(!map.containsKey(x)){
+
+        for (int e : map.keySet()) {
+            x = k - e;
+
+            if (e == 0) {
+                if (map.get(e) % 2 != 0) {
                     return false;
-                }else{
-                    \
-A                }
+                }
+            } else {
+                if (!map.containsKey(x)) {
+                    return false;
+                } else {
+                    int z = map.get(x);
+                    int m = map.get(e);
+
+                    if (z != m) {
+                        return false;
+                    }
+                }
             }
 
-        }
 
+        }
+        return true;
 
     }
 }
