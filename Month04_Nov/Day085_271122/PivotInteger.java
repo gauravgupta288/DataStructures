c39a78e5f4cf331975db4a2ffcf6d814d7704657
guptagaurav288 <guptagaurav288@gmail.com>
2022-11-27 11:45:11 +0530

    pivotInteger

---
+++ Month04_Nov/Day085_271122/PivotInteger.java
@@ -0,0 +1,34 @@
+package Month04_Nov.Day085_271122;
+
+public class PivotInteger {
+    public static void main(String[] args) {
+        System.out.println(pivotInteger(15));
+    }
+
+    public static int pivotInteger(int n) {
+
+        if(n==1){
+            return 1;
+        }
+        int i = 1;
+        int startSum = i;
+        int endSum = n;
+        while(i < n){
+            if(startSum < endSum){
+                startSum += ++i;
+            }else if(startSum > endSum){
+                endSum += --n;
+            }else if(startSum == endSum ){
+                if(i+1 == n-1){
+                    return i+1;
+
+                }else{
+                    startSum += ++i;
+                    endSum += --n;
+                }
+            }
+
+        }
+        return -1;
+    }
+}
