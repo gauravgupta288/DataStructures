6bc3a2febee2f28031e69b6ec32c8bef3d72e3b3
guptagaurav288 <guptagaurav288@gmail.com>
2022-11-27 22:59:02 +0530

    public class CountPairs {

--- Month04_Nov/Day085_271122/CountPairs.java
+++ Month04_Nov/Day085_271122/CountPairs.java
@@ -1,8 +1,12 @@
 package Month04_Nov.Day085_271122;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class CountPairs {
     public static void main(String[] args) {
-        System.out.println(countPairs(new int[]{3,2,1,5,4}, 2));
+        System.out.println(countPairsSol1(new int[]{1,2,2,1}, 1));
     }
 
     static int countPairs(int[] nums, int k){
@@ -18,6 +22,23 @@ public class CountPairs {
             }
         }
 
+        return count;
+    }
+
+    static int countPairsSol1(int arr[], int k){
+        int count = 0;
+        Map<Integer, Integer> map = new LinkedHashMap<>();
+        for(int i = 0 ; i < arr.length; i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+        }
+
+        int x = 0;
+        for(int i  : map.keySet()){
+            int z =  Math.abs(k - i);
+            if(map.containsKey(z) && x++ != i){
+                count += map.get(i) * map.get(z);
+            }
+        }
         return count;
     }
 }
