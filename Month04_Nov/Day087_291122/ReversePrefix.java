8d088a98e386dbdb5b8e98bd2e47117312866e2e
guptagaurav288 <guptagaurav288@gmail.com>
2022-11-29 18:11:45 +0530

    ReversePrefix

---
+++ Month04_Nov/Day087_291122/ReversePrefix.java
@@ -0,0 +1,29 @@
+package Month04_Nov.Day087_291122;
+
+public class ReversePrefix {
+
+    public static void main(String[] args) {
+        System.out.println(reversePrefix("abcdefd", 'd'));
+    }
+
+    static String reversePrefix(String word, char ch){
+
+        char chArr[] = word.toCharArray();
+        int index = word.indexOf(ch);
+
+        if(index == -1){
+            return word;
+        }else{
+            int start = 0;
+            while (start<index){
+                char temp = chArr[start];
+                chArr[start] = chArr[index];
+                chArr[index] = temp;
+                start++;
+                index--;
+            }
+        }
+
+        return String.valueOf(chArr);
+    }
+}
