ae6d7ae67911482737e8d6f0d731b93631dae91c
guptagaurav288 <guptagaurav288@gmail.com>
2022-11-29 17:50:50 +0530

    public class CountPairs {

--- Month04_Nov/Day086_281122/ArraneArray.java
+++ Month04_Nov/Day086_281122/ArraneArray.java
@@ -26,3 +26,4 @@ public class ArraneArray {
         return nums;
     }
 }
+
--- Month04_Nov/Day087_291122/StreamTest.java
+++ Month04_Nov/Day087_291122/StreamTest.java
@@ -1,12 +1,11 @@
 package Month04_Nov.Day087_291122;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
+import java.util.stream.*;
 
-public class Stream {
+public class StreamTest {
     public static void main(String[] args) {
         List<Integer> list = List.of(1,2,3,4,5,6);
-
         List<Integer> val = list.stream()
                 .map(i -> i * i)
                 .collect(Collectors.toList());
@@ -16,6 +15,11 @@ public class Stream {
         });
 
         System.out.println(val);
+
+        Stream<String> stream = Stream.of("gg", "bunny", "neha");
+
+        System.out.println(stream.anyMatch(x -> x.equals("ggg")));
+
     }
 
 }
