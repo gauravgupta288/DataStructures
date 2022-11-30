907b439c62044e1dd2e25da727e675ad886cc700
guptagaurav288 <guptagaurav288@gmail.com>
2022-11-30 19:15:09 +0530

    BrokenLetters

---
+++ Month04_Nov/Day088_301122/BrokenLetters.java
@@ -0,0 +1,30 @@
+package Month04_Nov.Day088_301122;
+
+public class BrokenLetters {
+    public static void main(String[] args) {
+        System.out.println(typeLetter("leet code", "ee"));
+    }
+
+    static int typeLetter(String text, String brokenLetters) {
+        int count = 0;
+
+        String[] word = text.split(" ");
+        boolean flag = false;
+
+        for (String s : word) {
+            for(char ch : brokenLetters.toCharArray()){
+                if(s.indexOf(ch) != -1){
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag){
+                count++;
+
+            }
+            flag = false;
+        }
+
+        return count;
+    }
+}
