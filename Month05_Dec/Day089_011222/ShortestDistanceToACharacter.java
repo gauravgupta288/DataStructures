9a6008e3a43c77206046737273dc59b9cc666495
guptagaurav288 <guptagaurav288@gmail.com>
2022-12-01 23:13:37 +0530

    shortestDistance

---
+++ Month05_Dec/Day089_011222/ShortestDistanceToACharacter.java
@@ -0,0 +1,31 @@
+package Month05_Dec.Day089_011222;
+
+import java.util.ArrayList;
+
+public class ShortestDistanceToACharacter {
+    public static void main(String[] args) {
+        shortestDistance("loveleetcode", 'e');
+    }
+
+    static int[] shortestDistance(String s, char c) {
+        ArrayList<Integer> al = new ArrayList<>();
+        int ans[] = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == c) {
+                al.add(i);
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            int j = 0;
+            int min = Integer.MAX_VALUE;
+
+            while (j < al.size()){
+                min = Math.min(Math.abs(al.get(j++) - i), min);
+            }
+            ans[i] = min;
+        }
+
+        return ans;
+    }
+}
