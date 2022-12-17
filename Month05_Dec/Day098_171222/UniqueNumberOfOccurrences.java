6b6245b684cb396757692fb99f8a4518cf3387dc
guptagaurav288 <guptagaurav288@gmail.com>
2022-12-17 13:26:30 +0530

    UniqueNumberOfOccurrences

---
+++ Month05_Dec/Day098_171222/UniqueNumberOfOccurrences.java
@@ -0,0 +1,27 @@
+package Month05_Dec.Day098_171222;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueNumberOfOccurrences {
+    public static void main(String[] args) {
+
+    }
+
+    static boolean uniqureOccurences(int arr[]){
+        Set<Integer> set = new HashSet<>();
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i : arr){
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        for(int i : map.values()){
+            set.add(i);
+        }
+
+        return map.size() == set.size();
+    }
+}
