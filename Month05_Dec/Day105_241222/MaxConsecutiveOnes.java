0f457fbabebed94876ae6f243943426abb533477
guptagaurav288 <guptagaurav288@gmail.com>
2022-12-24 20:57:09 +0530

    ContainingAll3Chars

---
+++ Month05_Dec/Day105_241222/MaxConsecutiveOnes.java
@@ -0,0 +1,18 @@
+package Month05_Dec.Day105_241222;
+
+public class MaxConsecutiveOnes {
+    public static void main(String[] args) {
+
+    }
+
+    static int maxOnes(int[] nums, int k) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if(nums[i] == 0){
+
+            }
+
+        }
+    }
+}
