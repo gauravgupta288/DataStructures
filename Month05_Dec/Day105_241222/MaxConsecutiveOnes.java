458242b123186b1f559f29f30922967323e1f9d4
guptagaurav288 <guptagaurav288@gmail.com>
2023-01-06 23:04:49 +0530

    ContainingAll3Chars

--- Month05_Dec/Day105_241222/MaxConsecutiveOnes.java
+++
@@ -1,6 +0,0 @@
-package Month05_Dec.Day105_241222;
-
-public class MaxConsecutiveOnes {
-    public static void main(String[] args) {
-    }
-}
---
+++ Month05_Dec/Day105_241222/test.java
@@ -0,0 +1,10 @@
+package Month05_Dec.Day105_241222;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("main");
+    }
+    static {
+        System.out.println("static");
+    }
+}
