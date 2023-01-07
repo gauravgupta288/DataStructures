6472b208f7838ca40be870ca0be5aca12a3df915
guptagaurav288 <guptagaurav288@gmail.com>
2023-01-07 13:44:26 +0530

    ContainingAll3Chars

--- Month05_Dec/Day105_241222/test.java
+++ Month05_Dec/Day105_241222/test.java
@@ -1,10 +1,34 @@
 package Month05_Dec.Day105_241222;
 
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 public class test {
-    public static void main(String[] args) {
-        System.out.println("main");
+    public static void main(String[] args) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode node = mapper.createObjectNode();
+        ObjectNode node2 = mapper.createObjectNode();
+
+        node.put("name", "xyz");
+        node.put("role", 1);
+
+        node2.put("name", "xyz");
+        node2.put("role", 2);
+
+        JsonNode tree1 = mapper.readTree(node.toString());
+        JsonNode tree2 = mapper.readTree(node2.toString());
+
+        if(tree1.equals(tree2)){
+            System.out.println("equals");
+        }else{
+            System.out.println("not equals");
+        }
     }
     static {
         System.out.println("static");
     }
+
 }
