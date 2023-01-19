d54c15c641780bf1b9e6dfa1abc73750bc955b03
guptagaurav288 <guptagaurav288@gmail.com>
2023-01-19 20:47:23 +0530

    WordsFormed

---
+++ Month06_Jan/Day109_190123/WordsFormed.java
@@ -0,0 +1,45 @@
+package Month06_Jan.Day109_190123;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordsFormed {
+    public static void main(String[] args) {
+        System.out.println(countWords(new String[]{"hello","world","leetcode"}, "welldonehoneyr"));
+    }
+
+    static int countWords(String[] words, String chars){
+        int count = 0;
+
+        Map<Character, Integer> map = new HashMap<>();
+        for(char ch : chars.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for(String s : words){
+            Map<Character, Integer> imap = new HashMap<>();
+            boolean flag = false;
+            for(char ch : s.toCharArray()){
+                imap.put(ch, imap.getOrDefault(ch, 0) + 1);
+            }
+
+            for(char ch : imap.keySet()){
+                if(!map.containsKey(ch)){
+                    flag = true;
+                    break;
+                }else {
+                    if(map.get(ch) < imap.get(ch)){
+                        flag = true;
+                        break;
+                    }
+                }
+
+            }
+            if(!flag){
+                count += s.length();
+            }
+
+        }
+        return count;
+    }
+}
