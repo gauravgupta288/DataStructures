c14405641a45ad4549904d86a7ab876080c90464
guptagaurav288 <guptagaurav288@gmail.com>
2023-02-16 13:06:54 +0530

    PrimeNumbers

---
+++ Month07_Feb/Day111_160223/PrimeNumbers.java
@@ -0,0 +1,28 @@
+package Month07_Feb.Day111_160223;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        System.out.println(prime(19));
+    }
+    public static int prime(int A){
+        int count = 0;
+
+        for(int i = 2;i <= A;i++){
+            if(isPrime(i)){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static boolean isPrime(int n){
+
+        for(int i = 2; i * i <= n; i++){
+            if(n%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
