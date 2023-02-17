e2042e02262efa4a568a9c92df58e8d58caa1c20
guptagaurav288 <guptagaurav288@gmail.com>
2023-02-17 17:04:59 +0530

    PrimeNumbers

---
+++ Month07_Feb/Day111_160223/PerfectSqaure.java
@@ -0,0 +1,25 @@
+package Month07_Feb.Day111_160223;
+
+public class PerfectSqaure {
+    public static void main(String[] args) {
+        System.out.println(perfectSquare(16));
+    }
+
+    public static boolean perfectSquare(int num) {
+
+        int start = 0;
+        int end = num;
+        while (start < end) {
+            int mid = (start + (end - start)) / 2;
+            long m = mid * mid;
+            if (m == num) {
+                return true;
+            } else if (m < num) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return false;
+    }
+}
