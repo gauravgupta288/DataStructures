dff0c6adcd4ceb82307415dee804159686ca2bab
guptagaurav288 <guptagaurav288@gmail.com>
2023-03-04 17:16:43 +0530

    ScalarProduct

---
+++ Month08_March/Day122_040323/TransposeMatrix.java
@@ -0,0 +1,39 @@
+package Month08_March.Day122_040323;
+
+public class TransposeMatrix {
+    public static void main(String[] args) {
+        System.out.println(solve(new int[][]{{1,2}, {1,2}, {1,2}}));
+    }
+
+    static int[][] solve(int[][] A){
+        int m = A[0].length;
+        int n = A.length;
+        int max = Math.max(A.length, A[0].length);
+
+        int result[][] = new int[max][max];
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                result[i][j] = A[i][j];
+            }
+        }
+
+        for(int i = 0; i < max; i++){
+            for(int j = 0; j < max; j++){
+                if(i < j){
+                    int temp = result[i][j];
+                    result[i][j] = result[j][i];
+                    result[j][i] = temp;
+                }
+            }
+        }
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                A[i][j] = result[i][j];
+            }
+        }
+
+        return A;
+    }
+}
