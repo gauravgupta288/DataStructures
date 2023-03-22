3da661e918677d40defaf80b1187a0946ce2c483
guptagaurav288 <guptagaurav288@gmail.com>
2023-03-22 22:39:42 +0530

    CountIncreasingTriplets

--- Month08_March/Day140_220323/CountIncreasingTriplets.java
+++ Month08_March/Day140_220323/CountIncreasingTriplets.java
@@ -2,7 +2,7 @@ package Month08_March.Day140_220323;
 
 public class CountIncreasingTriplets {
     public static void main(String[] args) {
-        System.out.println(solve(new int[]{4,1,2,6,9,7,2}));
+        System.out.println(solve(new int[]{4,1,2,6,9,7}));
     }
 
     static int solve(int[] A) {
@@ -11,21 +11,28 @@ public class CountIncreasingTriplets {
         int lside[] = new int[A.length];
         int rside[] = new int[A.length];
 
-        for (int i = 1; i < A.length; i++) {
-            if(A[i] > max){
-                max = A[i];
-                lside[i] = lside[i - 1] + 1;
-            }else{
-                lside[i] = lside[i - 1];
+        for (int i = 0; i < A.length; i++) {
+            int count = 0;
+            for(int j = 0; j < i; j++){
+                if(A[j] < A[i]){
+                    count++;
+                }
             }
+            lside[i] = count;
         }
 
-        for (int i = A.length - 2; i >= 0; i--) {
-            if(A[i] > A[i + 1]){
-                rside[i] = rside[i + 1] + 1;
-            }else{
-                rside[i] = rside[i + 1];
+        for (int i = A.length -  1; i >= 0; i--) {
+            int count = 0;
+            for(int j = A.length -  1; j > i; j--){
+                if(A[j] > A[i]){
+                    count++;
+                }
             }
+            rside[i] = count;
+        }
+
+        for (int i = 0; i < A.length; i++) {
+            sum += lside[i] * rside[i];
         }
 
         return sum;
