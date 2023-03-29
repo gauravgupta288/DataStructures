a30d40d8fd161803fd4ebc0d451b8cf80ed2b5b0
guptagaurav288 <guptagaurav288@gmail.com>
2023-03-29 09:26:01 +0530

    AddBinary

--- Month08_March/Day144_260323/AddBinary.java
+++ Month08_March/Day144_260323/AddBinary.java
@@ -1,57 +1,92 @@
 package Month08_March.Day144_260323;
 
-import java.util.ArrayList;
-
 public class AddBinary {
     public static void main(String[] args) {
-        System.out.println(addBinary("10001100010111000101100010100110001001101010000010011010", "101111000100100100111110010010101110101001100100101001111010011000000110"));
+        System.out.println(addBinary("1111", "1111"));
     }
 
     static String addBinary(String A, String B) {
-        char ch1[] = A.toCharArray();
-        char ch2[] = B.toCharArray();
-        int temp = 1;
-        long sum1 = 0;
-        long sum2 = 0;
+        StringBuilder sb = new StringBuilder();
 
-        for(int i = ch1.length - 1; i >= 0; i--){
-            if(ch1[i] == '1'){
-                sum1 += temp;
+        int carry = 0;
+        int i = A.length() - 1;
+        int j = B.length() - 1;
+        for(; i >= 0 && j >= 0; i--,j--){
+            char ch = A.charAt(i);
+            char ch1 = B.charAt(j);
+            if((ch == '0') && (ch1 == '0')){
+                if(carry == 0){
+                    sb.append('0');
+                    carry = 0;
+                }else{
+                    sb.append('1');
+                    carry = 0;
+                }
+            }else if((ch == '0') && (ch1 == '1') || (ch == '1') && (ch1 == '0')){
+                if(carry == 0){
+                    sb.append('1');
+                    carry = 0;
+                }else{
+                    sb.append('0');
+                    carry = 1;
+                }
+            }else if((ch == '1') && (ch1 == '1')){
+                if(carry == 0){
+                    sb.append('0');
+                    carry = 1;
+                }else{
+                    sb.append('1');
+                    carry = 1;
+                }
             }
-            temp *= 2;
         }
-
-        temp = 1;
-        for(int i = ch2.length - 1; i >= 0; i--){
-            if(ch2[i] == '1'){
-                sum2 += temp;
-            }
-            temp *= 2;
-        }
-
-        long res = sum1 + sum2;
-        ArrayList<Character> c = new ArrayList<>();
-
-        // Convert result to binary
-        while(res > 0){
-
-            long t = res % 2;
-            if(t == 1){
-                c.add('1');
+        while(i >=  0){
+            char ch = A.charAt(i);
+            if(ch == '0'){
+                if(carry == 1){
+                    sb.append('1');
+                    carry = 0;
+                }else{
+                    sb.append('0');
+                    carry = 0;
+                }
             }else{
-                c.add('0');
+                if(carry == 1){
+                    sb.append('0');
+                    carry = 1;
+                }else{
+                    sb.append('1');
+                    carry = 0;
+                }
             }
-
-            res = res >> 1;
+            i--;
         }
 
-        char ch3[] = new char[c.size()];
-
-        int k = 0;
-        for(int i = c.size() - 1; i >=0; i--){
-            ch3[k++ ] = c.get(i);
+        while(j >= 0){
+            char ch = B.charAt(j);
+            if(ch == '0'){
+                if(carry == 1){
+                    sb.append('1');
+                    carry = 0;
+                }else{
+                    sb.append('0');
+                    carry = 0;
+                }
+            }else{
+                if(carry == 1){
+                    sb.append('0');
+                    carry = 1;
+                }else{
+                    sb.append('1');
+                    carry = 0;
+                }
+            }
+            j--;
         }
-
-        return String.valueOf(ch3);
+        if(carry == 1){
+            sb.append('1');
+        }
+        return sb.reverse().toString();
     }
 }
+
