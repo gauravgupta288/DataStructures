ea59f23deab229daeb1f3f13b5632088b5140c22
guptagaurav288 <guptagaurav288@gmail.com>
2023-04-05 06:58:36 +0530

    Recursion

--- Month09_April/Day151_040423/Recursion.java
+++ Month09_April/Day151_040423/Recursion.java
@@ -2,8 +2,18 @@ package Month09_April.Day151_040423;
 
 public class Recursion {
     public static void main(String[] args) {
-        solve(9);
-        System.out.println();
+        System.out.println(fun(2,10));
+
+    }
+
+    static int fun(int x, int n){
+        if(n == 0){
+            return 1;
+        }else if(n % 2 == 0){
+            return fun(x * x, n / 2);
+        }else{
+            return x * fun(x * x, (n - 1) / 2);
+        }
     }
 
     static void solve(int A) {
