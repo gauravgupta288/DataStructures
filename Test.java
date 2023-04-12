dc74b6cf92eefd470df905371ee06ebd44f9fd4a
guptagaurav288 <guptagaurav288@gmail.com>
2023-04-12 19:27:35 +0530

    tree

--- Test.java
+++ Test.java
@@ -1,5 +1,43 @@
-class Test {
-    public static void main(String[] args) {
+import java.util.ArrayList;
 
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    public int[] preorderTraversal(TreeNode A) {
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        ans = pre(A, ans);
+
+        int[] n = new int[ans.size()];
+
+        int k = 0;
+        for(int i : ans){
+            n[k++] = i;
+        }
+
+        return n;
+    }
+
+    public ArrayList<Integer> pre(TreeNode A, ArrayList<Integer> ans){
+        if(A == null){
+            return null;
+        }
+
+        ans.add(A.val);
+        pre(A.left, ans);
+        pre(A.right, ans);
+
+        return ans;
     }
 }
