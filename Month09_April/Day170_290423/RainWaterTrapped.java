07cfb529b557e1eb6fa5742b6b0b4cae5e3813fc
guptagaurav288 <guptagaurav288@gmail.com>
2023-04-29 10:55:14 +0530

    RainWaterTrapped

--- Month09_April/Day170_290423/RainWaterTrapped.java
+++ Month09_April/Day170_290423/RainWaterTrapped.java
@@ -2,7 +2,7 @@ package Month09_April.Day170_290423;
 
 public class RainWaterTrapped {
     public static void main(String[] args) {
-        System.out.println(solve(new int[]{ 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+        System.out.println(solve(new int[]{ 56, 6, 52, 43, 23, 47, 48, 38, 96, 46, 30, 66, 80, 15, 62, 71, 61, 12, 98, 9, 28, 81, 70, 59, 95, 34, 9, 60, 70, 81, 71, 67, 58, 20, 22, 3, 95, 85, 20, 24, 74, 5, 23, 33, 75, 50, 38, 75, 68, 26, 46, 30, 75, 18, 4, 42, 51, 59, 8, 77}));
     }
 
     static int solve(int A[]){
@@ -16,7 +16,7 @@ public class RainWaterTrapped {
             lmax[i] = Math.max(lmax[i -1 ], A[i]);
         }
 
-        rmax[0] = A[A.length - 1];
+        rmax[A.length - 1] = A[A.length - 1];
         for(int i = A.length - 2; i >=0; i--){
             rmax[i] = Math.max(rmax[i + 1], A[i]);
         }
