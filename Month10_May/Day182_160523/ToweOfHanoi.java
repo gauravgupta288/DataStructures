73f4c66250f7f5edf829dc8444ab2f541c393c77
guptagaurav288 <guptagaurav288@gmail.com>
2023-05-16 17:45:18 +0530

    NCR using DP

---
+++ Month10_May/Day182_160523/ToweOfHanoi.java
@@ -0,0 +1,35 @@
+package Month10_May.Day182_160523;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToweOfHanoi {
+    public static void main(String[] args) {
+        System.out.println(towerOfHanoi(3));
+    }
+
+    static int[][] towerOfHanoi(int A) {
+
+        List<int[]> list = new ArrayList<>();
+        tower(A, 1, 3, 2, list);
+
+        int fans[][] = new int[list.size()][3];
+
+        for (int i = 0; i < list.size(); i++) {
+            fans[i] = list.get(i);
+        }
+        return fans;
+    }
+
+    static void tower(int disk, int start, int end, int temp, List<int[]> ans) {
+        if (disk == 1) {
+            ans.add(new int[]{disk, start, end});
+            return;
+        }
+
+        tower(disk - 1, start,temp, end,  ans);
+        ans.add(new int[]{disk, start, end});
+        tower(disk - 1, temp, end, start, ans);
+
+    }
+}
