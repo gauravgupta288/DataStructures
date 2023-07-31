58412d94ab0e4122db9e30231e6a7153c975b5e7
guptagaurav288 <guptagaurav288@gmail.com>
2023-07-31 08:38:08 +0530

    SimilarCharacter

---
+++ Month12_July/Day194_300723/EvaluateExpression.java
@@ -0,0 +1,57 @@
+package Month12_July.Day194_300723;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class EvaluateExpression {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("5");
+        list.add("1");
+        list.add("2");
+        list.add("+");
+        list.add("4");
+        list.add("*");
+        list.add("+");
+        list.add("3");
+        list.add("-");
+
+        System.out.println(solve(list));
+    }
+
+    public static int solve(ArrayList<String> A) {
+        Stack<String> st = new Stack<>();
+
+        for (String s : A) {
+
+            if (s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/")) {
+                String a = st.pop();
+                String b = st.pop();
+
+                st.push(Integer.toString(eval(Integer.parseInt(a), Integer.parseInt(b), s)));
+            }else{
+                st.push(s);
+            }
+        }
+
+
+
+        return Integer.parseInt(st.pop());
+
+    }
+
+    static int eval(Integer a, Integer b, String ope) {
+        if (ope.equals("+")) {
+            return a + b;
+        } else if (ope.equals("-")) {
+            return b - a;
+        } else if (ope.equals("*")) {
+            return a * b;
+        } else if (ope.equals("/")) {
+            return a / b;
+        } else {
+            return -1;
+        }
+    }
+}
