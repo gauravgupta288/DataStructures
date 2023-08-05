100205e91ef74a18747b72487715d7cd95ea32e6
guptagaurav288 <guptagaurav288@gmail.com>
2023-08-05 14:25:33 +0530

    KthSmallestInBST

---
+++ Month13_Aug/Day199_050823/KthSmallestInBST.java
@@ -0,0 +1,45 @@
+package Month13_Aug.Day199_050823;
+
+
+import java.util.ArrayList;
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) {
+       val = x;
+       left=null;
+       right=null;
+      }
+  }
+
+public class KthSmallestInBST {
+
+
+    public int kthsmallest(TreeNode A, int B) {
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        arr = mm(A, arr );
+
+        //System.out.println(arr);
+
+        return arr.get(B - 1);
+
+
+
+    }
+
+    ArrayList<Integer> mm(TreeNode A, ArrayList<Integer> arr){
+        if(A == null){
+            return null;
+        }
+
+        mm(A.left, arr);
+        arr.add(A.val);
+        mm(A.right, arr);
+
+        return arr;
+    }
+}
