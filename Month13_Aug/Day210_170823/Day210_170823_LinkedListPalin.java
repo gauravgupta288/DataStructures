4b22554911f8a2d976b923356f1424de74548e6c
guptagaurav288 <guptagaurav288@gmail.com>
2023-08-17 08:44:20 +0530

    Day210_170823_LinkedListPalin

---
+++ Month13_Aug/Day210_170823/Day210_170823_LinkedListPalin.java
@@ -0,0 +1,60 @@
+package Month13_Aug.Day210_170823;
+
+class ListNode{
+    ListNode next;
+    int val;
+}
+
+public class Day210_170823_LinkedListPalin {
+    public boolean isPalindrome(ListNode A) {
+
+        if(A == null){
+            return false;
+        }
+
+        ListNode k = A;
+
+        //Reach till middle of node
+
+        ListNode slow = A;
+        ListNode fast = A;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode temp = reverse(slow);
+
+        while(temp != null){
+
+            if(temp.val != k.val){
+                return false;
+            }
+            k = k.next;
+            temp = temp.next;
+        }
+
+        return true;
+
+    }
+
+    public ListNode reverse(ListNode A){
+
+        if(A == null || A.next == null){
+            return A;
+        }
+        ListNode pre = null;
+        ListNode cur = A;
+        ListNode head = A;
+
+        while(cur != null){
+            head = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = head;
+        }
+
+        return pre;
+    }
+}
