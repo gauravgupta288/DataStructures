3b9bda5edbaf8571bdde312a1a6c8e16b364f7e4
guptagaurav288 <guptagaurav288@gmail.com>
2023-09-02 11:02:20 +0530

    powerset2

---
+++ Month14_Sep/PowerSet2.java
@@ -0,0 +1,44 @@
+package Month14_Sep;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PowerSet2 {
+    public static void main(String[] args) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        powerset(ans, new int[]{1,1,2,2}, 0);
+
+        for(List<Integer> i : ans){
+            System.out.println(i);
+        }
+    }
+
+    public static void powerset(List<List<Integer>> ans, int nums[], int index){
+        if(index >= nums.length){
+            List<Integer> list = new ArrayList<>();
+            for(int i = 0; i < nums.length; i++){
+                list.add(nums[i]);
+            }
+            ans.add(list);
+            return;
+        }
+
+        for(int i = index; i< nums.length; i++){
+
+            if(i == index || nums[i] != nums[index]){
+                swap(nums, i, index);
+                powerset(ans, nums, index+1);
+                swap(nums, i, index);
+            }
+        }
+
+        return;
+    }
+
+    public static void swap(int nums[], int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
