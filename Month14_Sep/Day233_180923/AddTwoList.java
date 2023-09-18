a25bf0102c560739989ae1f2f5d7780c08c73fa8
guptagaurav288 <guptagaurav288@gmail.com>
2023-09-18 19:16:33 +0530

    sort list

---
+++ Month14_Sep/Day233_180923/AddTwoList.java
@@ -0,0 +1,19 @@
+package Month14_Sep.Day233_180923;
+
+
+import java.util.LinkedList;
+
+public class AddTwoList {
+    public static void main(String[] args) {
+        LinkedList<Integer> list1 = new LinkedList<>();
+        LinkedList<Integer> list2 = new LinkedList<>();
+
+        list1.add(2);
+        list1.add(4);
+        list1.add(3);
+        list1.add(5);
+        list1.add(6);
+        list1.add(4);
+    }
+}
+
