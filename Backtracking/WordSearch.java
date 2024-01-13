9e913983f25055789118818836ca0170af046a95
guptagaurav288 <guptagaurav288@gmail.com>
2024-01-13 22:14:16 +0530

    sudoku solve

--- Backtracking/WordSearch.java
+++ Backtracking/WordSearch.java
@@ -1,35 +1,57 @@
 package Backtracking;
 
 public class WordSearch {
+    static boolean check;
+
     public static void main(String[] args) {
-        System.out.println(solve(new char[][]{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "ABCCED", 0,0,0));
+        System.out.println(solve(new boolean[2][2], new char[][]{{'a','b'}, {'c','d'}}, "abcd", 0,0,0));
+
     }
 
-    public static boolean solve(char[][] board, String word, int index, int row, int col){
+    public static boolean solve(boolean arr[][], char[][] board, String word, int index, int row, int col){
 
+        if(word.length() == 1 && board.length == 1 && board[0].length == 1){
+            if(word.charAt(0) == board[0][0]){
+                return true;
+            }else{
+                return false;
+            }
+        }
         if(index >= word.length()){
+            check = true;
             return true;
         }
 
-        if(word.charAt(index) == board[row][col]){
+        if(row < 0 || row > board.length - 1 || col < 0 || col > board[0].length - 1) return false;
 
+        if(!arr[row][col] && word.charAt(index) == board[row][col]){
+            arr[row][col] = true;
             if(row > 0){
-                solve(board, word, index + 1, row - 1, col);
+                solve(arr, board, word, index + 1, row - 1, col);
             }
 
             if(row < board.length - 1){
-                solve(board, word, index + 1, row + 1, col);
+                solve(arr, board, word, index + 1, row + 1, col);
             }
 
             if(col > 0){
-                solve(board, word, index + 1, row, col - 1);
+                solve(arr, board, word, index + 1, row, col - 1);
             }
 
             if(col < board[0].length - 1){
-                solve(board, word, index + 1, row, col + 1);
+                solve(arr, board, word, index + 1, row, col + 1);
+            }
+
+            arr[row][col] = false;
+
+        }else{
+            if(col >= board[0].length - 1){
+                solve(arr, board, word, index, row+1, 0);
+            }else{
+                solve(arr, board, word, index, row, col+1);
             }
         }
 
-        return false;
+        return check;
     }
 }
