633dd374527ca6dcd9dcbaa96b326e3d9f1cceac
guptagaurav288 <guptagaurav288@gmail.com>
2024-03-19 23:05:02 +0530

    Factory

---
+++ Backtracking/List1.java
@@ -0,0 +1,24 @@
+package Backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class List1 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+        list.add(1);list.add(2);list.add(3);list.add(1);
+
+        System.out.println(list.contains(null));
+
+//        System.out.println(list.ge);
+    }
+
+    private List1(){
+
+    }
+}
+class test{
+//    List1 list1 = new List1();
+}
+
